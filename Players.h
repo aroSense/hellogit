dc7330145b97580d21a4df159ebbd348d99602fc
aroSense <aroSense@users.noreply.github.com>
2017-09-20 14:45:32 -0400

    Added header file

---
+++ Players.h
@@ -0,0 +1,16 @@
+#ifndef players_h
+#define players_h
+
+    class players
+{
+    private:
+        int PNum;
+        string PName;
+    
+    public:
+        player(string a, int b);
+        string getPName();
+        void setPName(string );
+        int getPNum();
+        void setPNum(int );
+};
