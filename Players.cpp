8ef7ca9758445e1821da5e8c7bd22bd3f99c71d1
aroSense <aroSense@users.noreply.github.com>
2017-09-20 14:56:41 -0400

    Added an implementation file

---
+++ Players.cpp
@@ -0,0 +1,26 @@
+#include "Players.h"
+	string Players :: getPName()
+	{
+		return PName;
+	}
+	
+	void Players :: setPName(string a)
+	{
+		PName = a;
+	}
+	
+	int Players :: getPNum()
+	{
+		return PNum;
+	}
+	
+	void Players :: setPNum(int b)
+	{
+		PNum = b;
+	}
+
+	Players :: Players(string a, int b)
+	{
+		string = a;
+		int =b;
+	}
